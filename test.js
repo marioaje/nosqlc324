65bf80d96cd84fa8f1650b2f39f95c18a6dadb2f
marioaje <marioaje@gmail.com>
2024-10-31 12:56:22 -0600

    parte 2

---
+++ test.js
@@ -0,0 +1,18 @@
+a = 9;
+console.log(a);
+
+// datosdecimales = 9.11;
+// console.log(datosdecimales);
+// operacionsuma = 9 +10;
+// operacionsumavariables = a +datosdecimales;
+
+// nombre = "Mario";
+// console.log(nombre);
+// b = true;
+// console.log(b);
+// declarararreglos = [12,true, "Nombres", 1.2];
+// console.log(declarararreglos);
+
+// declarardiccionariodatos = {
+//     "nombre": "Mario"
+// }
